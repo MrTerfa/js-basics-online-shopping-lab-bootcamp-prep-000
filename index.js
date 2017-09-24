222d6a6bcaa8247ba4e8953cbf3c28fc66435693
Terfa Tortema <terfa.tortema@gmail.com>
2017-09-24 18:47:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ function total() {
 
 function removeFromCart(item) {
   for(var i = 0; i < cart.length; i++){
-    var keys = Object.keys(cart[i])
     if(cart[i].hasOwnProperty(item)){
       cart.splice(i,1);
       return cart
